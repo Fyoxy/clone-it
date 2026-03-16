a7f9f672234e87df4257aa130926bd1c4f9f1b0f
emilkattel <emil1kattel@gmail.com>
2026-03-16 22:42:30 +0200

    pipboy with some options

--- game/zones/zone_wrist_ui.gd
+++ game/zones/zone_wrist_ui.gd
@@ -1,9 +1,15 @@
 extends PanelContainer
 
 
-func _on_save_button_pressed():
-	GameState.auto_save_game()
+func _on_rewind_button_button_down() -> void:
+	print("REWIND hehe")
+	pass # Replace with function body.
 
 
-func _on_quit_button_pressed():
-	GameState.quit_game()
+func _on_delete_button_button_down() -> void:
+	print("deleting da guy2")
+	pass # Replace with function body.
+
+
+func _on_quit_button_down() -> void:
+	get_tree().reload_current_scene()
