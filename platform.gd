4353600c7ea5fe962bf66c1c5768c0a0f8168e86
emilkattel <emil1kattel@gmail.com>
2026-03-21 20:12:53 +0200

    minor

--- platform.gd
+++ platform.gd
@@ -1,9 +1,9 @@
 extends BaseDoor
 @export var move_distance: float = 3
 @export var move_direction: Vector3 = Vector3(1, 0, 0)
+
 @onready var starting_pos: Vector3 = global_position
 @onready var forward_pos: Vector3 = starting_pos + move_direction * move_distance
-
 func open():
 	if _tween:
 		_tween.kill()
@@ -14,4 +14,4 @@ func close():
 	if _tween:
 		_tween.kill()
 	_tween = create_tween()
-	_tween.tween_property(self, "global_position", starting_pos, 1.0)
+	_tween.tween_property(self, "global_position", starting_pos, 3.0)
