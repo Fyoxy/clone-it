fb0dc17293a562c1d041f4ce4d14e62e7f7b66b2
emilkattel <emil1kattel@gmail.com>
2026-03-22 20:23:27 +0200

    chaged snap/orb destroyed sound

--- game/zones/seventh/ground_11.gd
+++ game/zones/seventh/ground_11.gd
@@ -3,6 +3,7 @@ extends StaticBody3D
 @export var trap: StaticBody3D
 @export var label_3d: Label3D
 @export var trap_crush: StaticBody3D
+@export var label_3d_2: Label3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,11 +16,12 @@ func _process(delta: float) -> void:
 
 func trapped():
 	var tween = create_tween()
-	tween.tween_property(self, "global_position:y", 2, 1)
+	tween.tween_property(self, "global_position:y", 2, 2)
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	trapped()
 	label_3d.show()
 	await get_tree().create_timer(2.0).timeout
+	label_3d_2.show()
 	trap_crush.crush()
--- game/zones/seventh/ground_9.gd
+++ game/zones/seventh/ground_9.gd
@@ -1,6 +1,5 @@
 extends StaticBody3D
 
-@onready var scene = preload("res://game/start_scene/start_scene.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,8 +11,11 @@ func _process(delta: float) -> void:
 	
 func crush():
 	var tween = create_tween()
-	tween.tween_property(self, "global_position:y",-5,5)
+	tween.tween_property(self, "global_position:y",-5,15)
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	get_tree().change_scene_to_file(scene.resource_path)
+	var path = "res://game/start_scene/start_scene.tscn"
+	var scene_base: XRToolsSceneBase = XRTools.find_xr_ancestor(self, "*", "XRToolsSceneBase")
+	if scene_base:
+		scene_base.load_scene(path)
